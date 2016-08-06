368c79ecd23392f35d5b7888b8428d7a7797386a
Riccardo Binetti <rbino@gmx.com>
2016-08-06 23:30:59 +0200

    main: register CartController QML type

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,13 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
+#include <QtQml>
+
+#include "CartController.h"
 
 int main(int argc, char *argv[])
 {
+    qmlRegisterType<CartController>("EmsQart", 1, 0, "CartController");
+
     QGuiApplication app(argc, argv);
     QQmlApplicationEngine engine(QUrl(QStringLiteral("qrc:/qml/MainView.qml")));
 
