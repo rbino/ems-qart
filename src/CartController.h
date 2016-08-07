800363b89f48848b671981fb8c819d7ca244248a
Riccardo Binetti <rbino@gmx.com>
2016-08-07 17:08:36 +0200

    CartController: remove CartHeaderOffsets enum
    It makes more sense in RomInfo

--- src/CartController.h
+++ src/CartController.h
@@ -12,20 +12,6 @@ class CartController : public QObject
     Q_PROPERTY(bool ready READ isReady NOTIFY readyChanged)
 
     public:
-        enum CartHeaderOffsets {
-            LogoOffset = 0x104,
-            TitleOffset = 0x134,
-            CGBFlagOffset = 0x143,
-            SGBFlagOffset = 0x146,
-            ROMSizeOffset = 0x148,
-            SRAMSizeOffset = 0x149,
-            RegionOffset = 0x14a,
-            OldLicenseOffset = 0x14b,
-            RomVersionOffset = 0x14c,
-            ChecksumOffset = 0x14d
-        };
-        Q_ENUM(CartHeaderOffsets)
-
         CartController(QObject *parent = nullptr);
 
         virtual ~CartController();
