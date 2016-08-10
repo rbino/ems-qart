fee10ce854365a025e81d6a666cc16df4e4170c2
Riccardo Binetti <rbino@gmx.com>
2016-08-10 17:07:56 +0200

    RomInfo: update romSize when updating the info

--- src/RomInfo.cpp
+++ src/RomInfo.cpp
@@ -28,6 +28,41 @@ void RomInfo::updateInfo(const QByteArray &header)
         m_checksumValid = true;
     }
     emit checksumValidChanged(m_checksumValid);
+
+    if (m_checksumValid) {
+        int sizeCode = header.at(ROMSizeOffset);
+        switch (sizeCode) {
+            case 0:
+            case 1:
+            case 2:
+            case 3:
+            case 4:
+            case 5:
+            case 6:
+            case 7:
+                m_romSize = 32 << (10 + sizeCode);
+                break;
+
+            case 0x52:
+                m_romSize = 1179648;
+                break;
+
+            case 0x53:
+                m_romSize = 1310720;
+                break;
+
+            case 0x54:
+                m_romSize = 1572864;
+                break;
+
+            default:
+                m_romSize = 0;
+        }
+
+    } else {
+        m_romSize = 0;
+    }
+    emit romSizeChanged(m_romSize);
 }
 
 void RomInfo::resetInfo()
