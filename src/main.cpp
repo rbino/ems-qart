31800079b75cd538a695bf78007bf198b8980bee
Riccardo Binetti <rbino@gmx.com>
2016-08-18 15:13:52 +0200

    main: set window icon
    This probably only has effect on Windows

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <QApplication>
 #include <QQmlApplicationEngine>
 #include <QtQml>
+#include <QIcon>
 
 #include "CartController.h"
 
@@ -9,6 +10,8 @@ int main(int argc, char *argv[])
     qmlRegisterType<CartController>("EmsQart", 1, 0, "CartController");
 
     QApplication app(argc, argv);
+    app.setWindowIcon(QIcon(QStringLiteral("qrc:/images/ems-qart.icon")));
+
     QQmlApplicationEngine engine(QUrl(QStringLiteral("qrc:/qml/MainView.qml")));
 
     return app.exec();
